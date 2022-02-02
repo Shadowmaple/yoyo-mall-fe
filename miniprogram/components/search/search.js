b68a5e41a71fd74155ea49f110be8c79fc1f1963
Shadowmaple <1142319190@qq.com>
2022-02-02 22:20:51 +0800

    :star: add search component

---
+++ miniprogram/components/search/search.js
@@ -0,0 +1,14 @@
+Component({
+  options: {},
+  data: {},
+  properties: {
+    // 默认显示的文字内容
+    text: {
+      type: String,
+      value: "请输入关键字"
+    },
+    // 跳转页面地址
+    src: String,
+  },
+  methods: {}
+})
