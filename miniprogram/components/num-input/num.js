c85b585d2e1367e597a1bf3759f4245046dc7184
Shadowmaple <1142319190@qq.com>
2022-02-06 20:22:20 +0800

    :star: update cart-page

---
+++ miniprogram/components/num-input/num.js
@@ -0,0 +1,74 @@
+Component({
+  options: {},
+  data: {
+    number: 1,
+    disabled1: false,
+    disabled2: false,
+  },
+  properties: {
+    min: {
+      type: Number,
+      value: 1,
+    },
+    max: {
+      type: Number,
+      value: 99,
+    },
+    default: {
+      type: Number,
+      value: 1,
+    }
+  },
+  lifetimes: {
+    attached: function() {
+      this.setData({
+        number: this.properties.default,
+        disabled1: this.data.number <= this.properties.min ? true : false,
+        disabled2: this.data.number >= this.properties.max ? true : false,
+      })
+    },
+  },
+  methods: {
+    nextNum: function(e) {
+      let curNum = this.data.number + 1
+      this.setData({
+        number: curNum,
+        disabled1: curNum <= this.properties.min ? true : false,
+        disabled2: curNum >= this.properties.max ? true : false,
+      })
+
+      this.changeNum()
+    },
+
+    preNum: function(e) {
+      let curNum = this.data.number - 1
+      this.setData({
+        number: curNum,
+        disabled1: curNum <= this.properties.min ? true : false,
+        disabled2: curNum >= this.properties.max ? true : false,
+      })
+
+      this.changeNum()
+    },
+
+    bindInput: function(e) {
+      let value = Number(e.detail.value)
+      value = value < this.properties.min ? this.properties.min : value
+      value = value > this.properties.max ? this.properties.max : value
+      this.setData({
+        number: value,
+        disabled1: value <= this.properties.min ? true : false,
+        disabled2: value >= this.properties.max ? true : false,
+      })
+
+      this.changeNum()
+    },
+
+    changeNum: function() {
+      var detail = {
+        num: this.data.number,
+      }
+      this.triggerEvent('changeNum', detail)
+    },
+  }
+})
--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -1,66 +1,141 @@
 // pages/cart/cart.js
+const app = getApp()
+const request = require("../../utils/request.js")
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    isEdit: false,
+    selectAll: false,
+    purchase: 0, // 当前需付费
+    discount: 0, // 优惠金额
+    cartList: [{
+      'id': 1,
+      'product_id': 42,
+      'title': '落叶飘零',
+      'author': '林则明',
+      'price': '20.20',
+      'cur_price': '18.50',
+      'image': 'https://img1.doubanio.com/view/subject/m/public/s2206907.jpg',
+      'num': 2,
+      'selected': false,
+      'isEdit': false, // 是否修改了数量
+    }],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
+    this.requestCart()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+  // 页面隐藏时保存商品数
   onHide: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  
+  // 下拉刷新
+  onPullDownRefresh: function() {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+  requestCart:function () {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  // 编辑商品事件
+  bindEdit: function (e) {
+    this.setData({
+      isEdit: !this.data.isEdit,
+    })
+  },
+
+  // 全选事件
+  bindSelectAll: function (e) {
+    let selectAll = !this.data.selectAll
+    let cartList = this.data.cartList
+    let purchase = new Number(0), discount = new Number(0)
+    for (let i in cartList) {
+      cartList[i].selected = selectAll
+      if (selectAll) {
+        purchase += cartList[i].cur_price
+        discount += cartList[i].price - cartList[i].cur_price
+      }
+    }
+
+    this.setData({
+      selectAll: selectAll,
+      purchase: purchase.toFixed(2),
+      discount: discount.toFixed(2),
+      cartList: cartList,
+    })
+  },
+
+  // 单选
+  bindSelect: function(e) {
+    let dataset = e.currentTarget.dataset
+    let idx = Number(dataset.idx)
+    let cartList = this.data.cartList
+    // 更新该商品的选择状态
+    cartList[idx].selected = !cartList[idx].selected
+    // 更新付费金额和优惠金额数目
+    let purchase = parseFloat(this.data.purchase)
+    let discount = parseFloat(this.data.discount)
+    if (cartList[idx].selected) {
+      purchase += cartList[idx].cur_price
+      discount += cartList[idx].price - cartList[idx].cur_price
+    } else {
+      purchase -= cartList[idx].cur_price
+      discount -= cartList[idx].price - cartList[idx].cur_price
+    }
+
+    // 更新全选状态
+    let flag = true
+    let selectedNum = 0, unselectedNum = 0
+    for (let i in cartList) {
+      if (cartList[i].selected) {
+        selectedNum++
+      } else {
+        unselectedNum++
+      }
+      if (selectedNum > 0 && unselectedNum > 0) {
+        flag = false
+        break
+      }
+    }
+    let selectAll = this.data.selectAll
+    if (flag) {
+      selectAll = selectedNum == cartList.length ? true : false
+    }
+
+    this.setData({
+      selectAll: selectAll,
+      purchase: purchase.toFixed(2),
+      discount: discount.toFixed(2),
+      cartList: cartList,
+    })
+  },
+
+  // 改变数量
+  bindChangeNum: function(e) {
+    let num = e.detail.num
+    let idx = e.currentTarget.dataset.idx
+    let cartList = this.data.cartList
+    cartList[idx].num = num
+    cartList[idx].isEdit = true
+    this.cartList = cartList
+  },
+
+  // 结算
+  bindConfirm: function (e) {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  // 批量移入收藏
+  bindBatchMoveToCollect: function(e) {
 
-  }
+  },
+
+  // 批量删除
+  bindBatchDel: function (e) {
+
+  },
 })
