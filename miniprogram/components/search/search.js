44fa20a64ee0d15f09b179c2abd46c38993b0fd3
Shadowmaple <1142319190@qq.com>
2022-02-14 22:28:22 +0800

    :star: update search page

--- miniprogram/components/search/search.js
+++ miniprogram/components/search/search.js
@@ -1,6 +1,8 @@
 Component({
   options: {},
-  data: {},
+  data: {
+    value: "",
+  },
   properties: {
     // 默认显示的文字内容
     text: {
@@ -9,21 +11,43 @@ Component({
     },
     // 点击是否跳转到指定页面
     isJump: {
-      type: Boolean,
-      value: true,
-    },
-    // 跳转页面地址
-    src: {
       type: String,
-      value: "/pages/search/search/search",
+      value: "true",
+    },
+    // 点击跳转页面地址
+    jumpSrc: {
+      type: String,
+      value: "",
+    },
+    // 输入确认跳转地址
+    confirmSrc: {
+      type: String,
+      value: "/pages/search/search_list/search_list"
     }
   },
   methods: {
-    clickClick: function(e) {
-      if (!this.properties.isJump) {
+    clickClick: function (e) {
+      if (this.properties.isJump != "true") {
         return
       }
-      let url = this.properties.src
+      let url = this.properties.jumpSrc
+      wx.navigateTo({
+        url: url,
+      })
+    },
+
+    clear: function (e) {
+      this.setData({
+        value: "",
+      })
+    },
+
+    bindConfirm: function (e) {
+      if (this.properties.confirmSrc == '') {
+        return
+      }
+      let value = e.detail.value
+      let url = this.properties.confirmSrc + '?value=' + value
       wx.navigateTo({
         url: url,
       })
